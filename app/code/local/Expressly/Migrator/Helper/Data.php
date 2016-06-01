3088b55fc9f7e5452d66bc8efdfb232a2d288545
Marc <marc.g.smith@gmail.com>
2016-06-01 18:25:50 +0100

    #8 - Remove old v1 code - https://github.com/expressly/magento/issues/8

--- app/code/local/Expressly/Migrator/Helper/Data.php
+++
@@ -1,4 +0,0 @@
-<?php
-class Expressly_Migrator_Helper_Data extends Mage_Core_Helper_Abstract {
-}
-?>
--- app/code/local/Expressly/Migrator/Model/Resource/Setup.php
+++
@@ -1,4 +0,0 @@
-<?php
-class Expressly_Migrator_Model_Resource_Setup extends Mage_Core_Model_Resource_Setup {
-}
-?>
